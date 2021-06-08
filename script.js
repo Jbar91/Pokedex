8cc6cc09500b6a78f09a8acf82d406ee51bfd796
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-06-08 18:50:53 +0200

    Changed the way of variable  creation after calling the pokemon api and added some comments

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const cardsContainer = document.querySelector("#pokemon-display");
 
 function fetchKanto() {
+  //Fetch pokemon data from api with a limit of 151
   fetch("https://pokeapi.co/api/v2/pokemon/?limit=151&?order=1")
     .then((Response) => Response.json())
     .then((allPokemon) => {
+      //Iteration for each pokemon to bring the data and create a display
       allPokemon.results.forEach((pokemon) => {
-        //console.log(pokemon);
         fetchPokemonData(pokemon);
         function fetchPokemonData(pokemon) {
+          //Specific pokemon data
           let url = pokemon.url;
+          //Creates the card of each pokemon and sets an ID with its name
           cardsContainer.innerHTML += `<div class="card" id="${pokemon.name}"></div>`;
           fetch(url)
             .then((Response) => Response.json())
             .then((pokeData) => {
-              const card = document.getElementById(`${pokemon.name}`);
-              const name = pokeData.name;
-              const types = pokeData.types;
-              const pokedexNumber = pokeData.id;
-              const sprite = pokeData.sprites.front_default;
+              //Destructuring to create the variables to be used
+              const [card, name, types, pokedexNumber, sprite] = [
+                document.getElementById(`${pokemon.name}`),
+                pokeData.name,
+                pokeData.types,
+                pokeData.id,
+                pokeData.sprites.front_default,
+              ];
+              //Adding the name, pokedex number, type(s) and sprites to the cards
               card.innerHTML += `<img src="${sprite}" class="sprite"><p class="number">${pokedexNumber}</p><h2 class="name">${name.toUpperCase()}</h2>${
                 types.length === 1
                   ? `<span class="type pokemon-type-${
@@ -34,4 +41,5 @@ function fetchKanto() {
       });
     });
 }
+//Invoking the main function
 fetchKanto();
